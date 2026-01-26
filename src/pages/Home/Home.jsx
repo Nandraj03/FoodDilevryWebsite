0a4f2439980aa58123a71beb25009d365eb7ec04
Snehnand <nandrajgore3@gmail.com>
2026-01-26 16:42:33 +0530

    change css of  header and navbar component create ExplorMenu component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../component/Header/Header'
+import ExploreMenu from '../../component/ExploreMenu/ExploreMenu'
 const Home = () => {
+
+    const [category,setCategory] = useState("All")
+
+
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
       </div>
   )
 }
